20867c7d041706485c8db32af3ff8f16f5cd7f1e
Ruben Oussoren <ruben.oussoren1@gmail.com>
2023-06-15 13:09:59 -0400

    Delete gpt_context directory

--- gpt_context/extract_opal_files.rb
+++
@@ -1,5 +0,0 @@
-require 'opal'
-
-File.write('vendor/opal.js', Opal::Builder.build('opal'))
-File.write('vendor/opal-parser.js', Opal::Builder.build('opal-parser'))
-File.write('opal_generated/content_script.js', Opal.compile(File.read('content_script.rb')))
